bf3f367279729078b1281964a5d2abf4fb05e796
Hyunkyung-Nam <gusrud13579@gmail.com>
2024-05-30 14:57:00 +0900

    feat : 키워드, 연령, 저자 별 도서 추천 및 인기도서 검색 구현

--- routes/query.js
+++ routes/query.js
@@ -3,6 +3,5 @@ const controller = require("../controller/query");
 const router = express.Router();
 
 router.post("/textQuery", controller.textQuery);
-router.post("/eventQuery", controller.eventQuery);
 
 module.exports = router;
